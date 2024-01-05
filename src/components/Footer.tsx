5a913fcec5ac8676b51b73779935d7b22d593e96
Yang Zhi <yangzhi.see@gmail.com>
2024-01-05 20:52:07 +0700

    MVP

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <ContainerInner>
             <p className="text-center text-sm text-zinc-400 dark:text-zinc-500">
-              &copy; {new Date().getFullYear()} See Yang Zhi. All rights
+              &copy; {new Date().getFullYear()} NUS GPA Calculator. All rights
               reserved.
             </p>
           </ContainerInner>
